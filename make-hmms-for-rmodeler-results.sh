a0381ac89d3c9a13c3028248050afadc0bcd1397
Malte Petersen <mptrsen@uni-bonn.de>
2017-07-07 17:18:05 +0200

    Add wrapper script for cool HMMbuilder

---
+++ make-hmms-for-rmodeler-results.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+N=$(grep -c -v '^#' doc/repeatmodeler-dirs.txt)
+
+declare -i i=0
+
+grep -v '^#' doc/repeatmodeler-dirs.txt | while read SPECIES DIR; do
+	let i++
+	NSEQS=$(grep -c '>' $DIR/consensi.fa.classified)
+	echo "# Making HMMS for $SPECIES ($i/$N, $NSEQS seqs)"
+	mkdir -p results/hmms/$SPECIES
+	perl code/fetch-families-from-repeatmodeler-consensi.pl --ncpu 7 --outdir results/hmms/$SPECIES $DIR > results/hmms/$SPECIES.log
+	cat results/hmms/$SPECIES/*.hmm > results/hmms/$SPECIES.hmm
+	rm results/hmms/$SPECIES/*.afa
+done
+echo "# All done"
